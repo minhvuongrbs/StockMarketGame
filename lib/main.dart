e291209cdf6fbc2edc8547be03e72364a679440a
minhvuong <minhvuong2810@gmail.com>
2019-05-24 19:57:03 +0700

    change new diagram type

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
      SystemChrome.setPreferredOrientations(
       [
         DeviceOrientation.landscapeLeft,
--- lib/src/widgets/custom_widget/stock_line_chart.dart
+++ lib/src/widgets/custom_widget/stock_line_chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class StockLineChart extends StatefulWidget {
   @override
@@ -9,8 +10,41 @@ class StockLineChart extends StatefulWidget {
 class _StockLineChartState extends State<StockLineChart> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("stockline chart"),
+    return Expanded(
+          child: charts.TimeSeriesChart(
+            _createSampleData(),
+            animate: false,
+            primaryMeasureAxis: new charts.NumericAxisSpec(
+            tickProviderSpec:
+                new charts.BasicNumericTickProviderSpec(desiredTickCount: 2))
+          )
     );
   }
+  static List<charts.Series<TimeSeriesStocks, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesStocks(new DateTime(2017, 9, 19), 5),
+      new TimeSeriesStocks(new DateTime(2017, 9, 26), 25),
+      new TimeSeriesStocks(new DateTime(2017, 10, 3), 100),
+      new TimeSeriesStocks(new DateTime(2017, 10, 10), 41),
+      new TimeSeriesStocks(new DateTime(2017, 10, 10), 75),
+      new TimeSeriesStocks(new DateTime(2017, 10, 10), 35),
+      new TimeSeriesStocks(new DateTime(2017, 10, 10), 12),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesStocks, DateTime>(
+        id: 'stock',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesStocks stocks, _) => stocks.time,
+        measureFn: (TimeSeriesStocks stocks, _) => stocks.stocks,
+        data: data,
+      )
+    ];
+  }
+}
+class TimeSeriesStocks {
+  final DateTime time;
+  final int stocks;
+
+  TimeSeriesStocks(this.time, this.stocks);
 }
