77b0ebc6f765681f807841b2213f4f7673d64fa6
Au Le <lethehaiau@gmail.com>
2019-05-25 13:18:28 +0900

    add animation to simulate real time data

--- lib/src/widgets/custom_widget/stock_line_chart.dart
+++ lib/src/widgets/custom_widget/stock_line_chart.dart
@@ -2,22 +2,34 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/src/text_element.dart';
 import 'package:charts_flutter/src/text_style.dart' as style;
-import 'package:charts_flutter/flutter.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class StockLineChart extends StatefulWidget {
   @override
   _StockLineChartState createState() => _StockLineChartState();
 }
 
-
-class _StockLineChartState extends State<StockLineChart> {
-  
+class _StockLineChartState extends State<StockLineChart>
+    with SingleTickerProviderStateMixin {
   List<TimeSeriesStocks> selectionData = [];
+  AnimationController controller;
+  Animation<int> _datasize;
 
-  _onSelectionChanged(SelectionModel model) {
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 10), vsync: this);
+    _datasize = IntTween(begin: 0, end: 6).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward();
+  }
+
+  _onSelectionChanged(charts.SelectionModel model) {
     final selectedDatum = model.selectedDatum;
     TimeSeriesStocks selectedPoint;
-    bool ok = false;
     // We get the model that updated with a list of [SeriesDatum] which is
     // simply a pair of series & datum.
     //
@@ -30,38 +42,39 @@ class _StockLineChartState extends State<StockLineChart> {
 
     // Request a build.
     setState(() {
-        selectionData.add(selectedPoint);
+      selectionData.add(selectedPoint);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
-          child: TimeSeriesChart(
-            _createSampleData(selectionData),
-            animate: false,
-            primaryMeasureAxis: new NumericAxisSpec(
-            tickProviderSpec:
-                BasicNumericTickProviderSpec()),
-            customSeriesRenderers: [
-              PointRendererConfig(
-                customRendererId: 'custom point',
-                symbolRenderer: CustomCircleSymbolRenderer()
-              )
-            ],
-            selectionModels: [
-              SelectionModelConfig(
-                changedListener: _onSelectionChanged,
-              )
-            ],
-            dateTimeFactory: const LocalDateTimeFactory(),
-          )
-    );
+        child: charts.TimeSeriesChart(
+      _createSampleData(selectionData),
+      animate: false,
+      primaryMeasureAxis: new charts.NumericAxisSpec(
+          tickProviderSpec: charts.BasicNumericTickProviderSpec()),
+      customSeriesRenderers: [
+        charts.PointRendererConfig(
+          customRendererId: 'custom point',
+          symbolRenderer: CustomCircleSymbolRenderer(label: 'User'),
+        )
+      ],
+      selectionModels: [
+        charts.SelectionModelConfig(
+          changedListener: _onSelectionChanged,
+        )
+      ],
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
+    ));
   }
-  static List<Series<TimeSeriesStocks, DateTime>> _createSampleData(List<TimeSeriesStocks> selectionData) {
+
+  List<charts.Series<TimeSeriesStocks, DateTime>> _createSampleData(
+      List<TimeSeriesStocks> selectionData) {
     final data = [
       new TimeSeriesStocks(new DateTime(2017, 9, 19), 5),
       new TimeSeriesStocks(new DateTime(2017, 9, 26), 30),
-      new TimeSeriesStocks(new DateTime(2017, 10, 3), 100),
+      new TimeSeriesStocks(new DateTime(2017, 10, 3), 110),
       new TimeSeriesStocks(new DateTime(2017, 10, 10), 41),
       new TimeSeriesStocks(new DateTime(2017, 10, 11), 75),
       new TimeSeriesStocks(new DateTime(2017, 10, 12), 35),
@@ -69,39 +82,51 @@ class _StockLineChartState extends State<StockLineChart> {
     ];
 
     return [
-      new Series<TimeSeriesStocks, DateTime>(
+      new charts.Series<TimeSeriesStocks, DateTime>(
         id: 'stock',
-        colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         domainFn: (TimeSeriesStocks stocks, _) => stocks.time,
         measureFn: (TimeSeriesStocks stocks, _) => stocks.stocks,
-        data: data,
+        data: data.sublist(0, _datasize.value),
       ),
-      new Series<TimeSeriesStocks, DateTime>(
+      new charts.Series<TimeSeriesStocks, DateTime>(
         id: 'select point',
-        colorFn: (_, __) => MaterialPalette.pink.shadeDefault,
+        colorFn: (_, __) => charts.MaterialPalette.purple.shadeDefault,
+        fillColorFn: (_, __) => charts.MaterialPalette.white,
+        strokeWidthPxFn: (_, __) => 2.0,
         domainFn: (TimeSeriesStocks stocks, _) => stocks.time,
-        measureFn: (TimeSeriesStocks stocks, _) => stocks.stocks + 2,
+        measureFn: (TimeSeriesStocks stocks, _) => stocks.stocks,
         data: selectionData,
-      )..setAttribute(rendererIdKey, 'custom point')
+      )..setAttribute(charts.rendererIdKey, 'custom point')
     ];
   }
 }
-class CustomCircleSymbolRenderer extends CircleSymbolRenderer {
+
+class CustomCircleSymbolRenderer extends charts.CircleSymbolRenderer {
+  final String label;
+  CustomCircleSymbolRenderer({this.label});
   @override
-  void paint(ChartCanvas canvas, Rectangle<num> bounds, {List<int> dashPattern, Color fillColor, Color strokeColor, double strokeWidthPx}) {
-    super.paint(canvas, bounds, dashPattern: dashPattern, fillColor: fillColor, strokeColor: strokeColor, strokeWidthPx: strokeWidthPx);
+  void paint(charts.ChartCanvas canvas, Rectangle<num> bounds,
+      {List<int> dashPattern,
+      charts.Color fillColor,
+      charts.Color strokeColor,
+      double strokeWidthPx}) {
+    super.paint(canvas, bounds,
+        dashPattern: dashPattern,
+        fillColor: fillColor,
+        strokeColor: strokeColor,
+        strokeWidthPx: strokeWidthPx);
     canvas.drawRect(
-      Rectangle(bounds.left - 5, bounds.top - 30, bounds.width + 10, bounds.height + 10),
-      fill: Color.white
-    );
+        Rectangle((bounds.left + bounds.width / 2 - label.length * 3.7).round(),
+            bounds.top - 30, label.length * 8, bounds.height + 10),
+        fill: charts.Color(r: 52, g: 73, b: 94, a: 30));
     var textStyle = style.TextStyle();
-    textStyle.color = Color.black;
+    textStyle.color = charts.Color.black;
     textStyle.fontSize = 15;
     canvas.drawText(
-      TextElement("sell", style: textStyle),
-        (bounds.left).round(),
-        (bounds.top - 28).round()
-    );
+        TextElement(label, style: textStyle),
+        (bounds.left + bounds.width / 2 - label.length * 3.5).round(),
+        (bounds.top - 28).round());
   }
 }
 
