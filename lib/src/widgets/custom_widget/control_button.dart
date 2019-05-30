4836210d23db182adbf8aef6dc750a75b5ec22cb
minhvuong <minhvuong2810@gmail.com>
2019-05-30 23:15:46 +0700

    init ui

--- lib/src/widgets/custom_widget/control_button.dart
+++ lib/src/widgets/custom_widget/control_button.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+
 class ControlButton extends StatefulWidget {
- @override  
+  @override
   _ControlButtonState createState() => _ControlButtonState();
 }
+
 class _ControlButtonState extends State<ControlButton> {
-  @override  Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text("control button"),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _startSection(),
+          _purchaseSection(),
+          _scoreBoardSection(),
+        ],
+      ),
+    );
+  }
+
+  Widget _startSection() {
+    return RaisedButton(
+      child: Text("Start"),
+      onPressed: () {},
+      color: Colors.green,
+    );
+  }
+
+  Widget _purchaseSection() {
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          child: Text("Buy"),
+          onPressed: () {},
+          color: Colors.blueAccent,
+        ),
+        RaisedButton(
+          child: Text("Cell"),
+          onPressed: () {},
+          color: Colors.redAccent,
+        ),
+      ],
+    );
+  }
+
+  Widget _scoreBoardSection() {
+    return Column(
+      children: <Widget>[
+        Text("AI: 123"),
+        Text("User: 234"),
+      ],
     );
   }
 }
